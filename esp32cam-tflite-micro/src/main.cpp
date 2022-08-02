65e11960f1f4ac000c4cda35b83b6d3f0ffaf86b
burgerballs <insanederson@gmail.com>
2022-08-01 17:00:27 -0700

    Mnist example

--- esp32cam-tflite-micro/src/main.cpp
+++ esp32cam-tflite-micro/src/main.cpp
@@ -3,16 +3,91 @@
 #include "digit.h"
 #include "model_data.h"
 
+#include "esp_camera.h"
+#include "FS.h"                // SD Card ESP32
+#include "SD_MMC.h"            // SD Card ESP32
+#include "soc/soc.h"           // Disable brownour problems
+#include "soc/rtc_cntl_reg.h"  // Disable brownour problems
+#include "driver/rtc_io.h"
+#include "esp32_ov2640.h"
+#include "esp32_sd.h"
+
 TFLM_Net *nn;
+camera_fb_t * fb = NULL;
 
 void setup()
 {
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+  if (!camera_init()) return;
+  if (!sd_init()) return;
+
   Serial.begin(115200);
   nn = new TFLM_Net(converted_model_tflite, 1028*60);
+
+  fb = esp_camera_fb_get();  
+  esp_camera_fb_return(fb);
 }
+
+int counter = 0;
+float downscale[784];
+int value[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
 void loop()
 {
+  fb = esp_camera_fb_get();  
+  if (!fb) 
+  {
+    Serial.println("Camera capture failed");
+  }
+  else
+  {
+    //Serial.println(fb->len);
+  }
+
+  int scale = 4;
+  for (int row = 0; row < 96; row += scale)
+    for (int col = 0; col < 96; col += scale)
+    {
+      if (fb->buf[col + row*96] > 95)
+        downscale[(int)(floor(col/scale) + floor(row*28/scale))] = 0.0;
+      else
+        downscale[(int)(floor(col/scale) + floor(row*28/scale))] = (255 - fb->buf[col + row*96]) / 255.0;
+        
+    }
+  nn->load_input(downscale, 784);
+  nn->run();
+
+  float max = 0;
+  int max_index = 0;
+
+  for (size_t i = 0; i < 10; i++)
+  {
+    Serial.print(nn->output_float()[i]);
+    Serial.print(" ");
+    if (nn->output_float()[i] > max)
+    {
+      max_index = i;
+      max = nn->output_float()[i];
+    }
+  }
+  Serial.println(value[max_index]);
+  Serial.println('\n');
+  /*
+  for (int row = 0; row < 28; row++)
+  {
+    for (int col = 0; col < 28; col++)
+    {
+      Serial.print(downscale[col + row*28]);
+      Serial.print(',');
+    }
+    Serial.println();
+  }
+  */
+  //sd_write("/" + String(counter++) + ".bmp", bmp, fb->len * 3);
+  esp_camera_fb_return(fb);
+
+  delay(500);
+  /*
   nn->load_input(digit, 784);
   nn->run();
 
@@ -22,6 +97,7 @@ void loop()
     Serial.print(", ");
   }
   Serial.println('\n');
+  */
 
   /*
   float number1 = random(100) / 100.0;
@@ -38,5 +114,4 @@ void loop()
 
   Serial.printf("%.2f %.2f - result %.2f - Expected %s, Predicted %s\n", number1, number2, result, expected, predicted);
 */
-  delay(1000);
 }
