378e1f8382456428d5390f617199a10118439911
burgerballs <insanederson@gmail.com>
2022-08-09 23:41:43 -0700

    Lab datasets

--- eps32cam-camera-server/src/main.cpp
+++ eps32cam-camera-server/src/main.cpp
@@ -16,8 +16,8 @@
 #include "driver/rtc_io.h"
 
 //Replace with your network credentials
-const char* ssid = "err404_guest_not_found";
-const char* password = "GoBruinsTrackandField!";
+const char* ssid = "HiLab";
+const char* password = "UCLAhilab21";
 
 #define PART_BOUNDARY "123456789000000000000987654321"
 
@@ -108,94 +108,12 @@ const char* password = "GoBruinsTrackandField!";
   #error "Camera model not selected"
 #endif
 
-static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
-static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
-static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
-
-httpd_handle_t stream_httpd = NULL;
-
-static esp_err_t stream_handler(httpd_req_t *req){
-  camera_fb_t * fb = NULL;
-  esp_err_t res = ESP_OK;
-  size_t _jpg_buf_len = 0;
-  uint8_t * _jpg_buf = NULL;
-  char * part_buf[64];
-
-  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
-  if(res != ESP_OK){
-    return res;
-  }
-
-  while(true){
-    fb = esp_camera_fb_get();
-    if (!fb) {
-      Serial.println("Camera capture failed");
-      res = ESP_FAIL;
-    } else {
-      if(fb->format != PIXFORMAT_JPEG){
-        bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
-        esp_camera_fb_return(fb);
-        fb = NULL;
-        if(!jpeg_converted){
-          Serial.println("JPEG compression failed");
-          res = ESP_FAIL;
-        }
-      } else {
-        _jpg_buf_len = fb->len;
-        _jpg_buf = fb->buf;
-      }
-    }
-    if(res == ESP_OK){
-      size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
-      res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
-    }
-    if(res == ESP_OK){
-      res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
-    }
-    if(res == ESP_OK){
-      res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
-    }
-    if(fb){
-      esp_camera_fb_return(fb);
-      fb = NULL;
-      _jpg_buf = NULL;
-    } else if(_jpg_buf){
-      free(_jpg_buf);
-      _jpg_buf = NULL;
-    }
-    if(res != ESP_OK){
-      break;
-    }
-    //Serial.printf("MJPG: %uB\n",(uint32_t)(_jpg_buf_len));
-  }
-  return res;
-}
-
-void startCameraServer(){
-  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
-  config.server_port = 80;
-
-  httpd_uri_t index_uri = {
-    .uri       = "/",
-    .method    = HTTP_GET,
-    .handler   = stream_handler,
-    .user_ctx  = NULL
-  };
-  
-  //Serial.printf("Starting web server on port: '%d'\n", config.server_port);
-  if (httpd_start(&stream_httpd, &config) == ESP_OK) {
-    httpd_register_uri_handler(stream_httpd, &index_uri);
-  }
-}
-
 void setup() {
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
  
   Serial.begin(115200);
   Serial.setDebugOutput(false);
   Serial.println("Start");
-  pinMode(13, INPUT_PULLDOWN);
-  pinMode(12, OUTPUT);
   if (!sd_init()) return;
   //digitalWrite(12, HIGH);
   
@@ -280,17 +198,17 @@ void loop() {
   size_t out_len;
   // subtract 54
   //frame2bmp(_fb, &out, &out_len);
-  Serial.println(out_len);
+  Serial.println(_fb->len);
 
   delay(1000);
-  sd_write("/" + String(counter++) + ".jpg", _fb->buf, _fb->len);
+  if (counter++ >= 3)
+    sd_write("/" + String(counter - 3) + ".jpg", _fb->buf, _fb->len);
 
   //free(out);
   esp_camera_fb_return(_fb);
-  Serial.println("===============");
-  delay(2000);
+  //Serial.println("===============");
 
-  if (counter == 102)
+  if (counter == 113)
     while(1);
   
   //delay(1);
